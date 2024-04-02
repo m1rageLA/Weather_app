ee4083eaa67c1c2cb7fc11b17a3fc909a969dc84
m1rageLA <miragela.code@gmail.com>
2024-04-02 14:36:08 +0200

    Init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import './assets/main.scss'
+import type InfoBlock from './components/InfoBlock.vue'
+</script>
+
+<template>
+  <div>
+    <main>
+      <InfoBlock class="test" />
+    </main>
+  </div>
+
+  <RouterView />
+</template>
+
+<style scoped>
+body {
+  background: rgb(228, 174, 238);
+  background: radial-gradient(circle, rgba(228, 174, 238, 1) 0%, rgba(148, 187, 233, 1) 100%);
+}
+</style>
---
+++ src/components/InfoBlock.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="test">dd</div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'InfoBlock'
+}
+</script>
